1df6bcbe464be9293c222489d3d2abf5a48accd2
Jaemyeong Jin <jaemyeong@me.com>
2023-01-11 20:59:40 +0900

    Closed #22

--- build.sh
+++ build.sh
@@ -4,14 +4,20 @@ set -e
 
 VERSION="release/3.16.1"
 
-git clone --depth=1 --branch="${VERSION}" https://github.com/navermaps/NMapsMap.git
+# git clone --depth=1 --branch="${VERSION}" https://github.com/navermaps/NMapsMap.git
+
+curl --location -o NMapsMap.zip "https://github.com/navermaps/NMapsMap/releases/download/${VERSION}/NMapsMap.framework.zip"
+
+unzip NMapsMap.zip -d NMapsMap
+
+rm NMapsMap.zip
 
 mv NMapsMap/framework/NMapsMap.xcframework NMapsMap.xcframework
 
-zip -r NMapsMap.xcframework.zip NMapsMap.xcframework
+zip -r NMapsMap.xcframework.zip NMapsMap.xcframework -x \*.DS_Store
 
-rm -rf NMapsMap.xcframework
+rm -r NMapsMap.xcframework
 
-rm -rf NMapsMap
+rm -r NMapsMap
 
 swift package compute-checksum NMapsMap.xcframework.zip
